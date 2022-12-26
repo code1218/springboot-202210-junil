b1fed21a085d359f1b177d773d9392c99ef7da10
code1218 <skjil1218@gmail.com>
2022-12-26 21:55:36 +0900

    validation 시작, 정규식

--- src/main/java/com/study/springboot202210junil/web/dto/CategoryDto.java
+++ src/main/java/com/study/springboot202210junil/web/dto/CategoryDto.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class CategoryDto {
+
     private int categoryId;
     private String categoryName;
     private String categoryOpt1;
